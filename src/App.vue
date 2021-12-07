1b09d20ba7afdacadadccc98b518185b32ca83ce
TiTUS <us.daniel.roberts@gmail.com>
2021-12-08 00:44:33 +0400

    update App

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Fuzzy+Bubbles&display=swap");
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  font-family: "Fuzzy Bubbles", cursive;
   text-align: center;
   color: #2c3e50;
 }
---
+++ src/views/Lobby.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="lobby">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Lobby",
+  components: {
+  },
+};
+</script>
+
+<style lang="scss"></style>
